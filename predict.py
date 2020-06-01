8ef6fd6d13ceeadc8c33852ca75d1fd2d5e8fa55
zloeffle <40178843+zloeffle@users.noreply.github.com>
2020-05-31 22:10:53 -0500

    dt updates

--- predict.py
+++ predict.py
@@ -52,16 +52,21 @@ class Signal:
         self.results = None           
     
     '''
-    generate features for the decision tree
+    generate features
     '''
     def create_features(self,end=None):
+        # pull stocks from db
         query = 'SELECT Ticker FROM stocks.collections WHERE Collection = "100-most-popular"'
         df = pd.read_sql(query,con=client.database.connection)
+        
+        # set ticker as df index
         df.set_index('Ticker',inplace=True)
         tickers = list(df.index)
         
-        rsi,gc,start_prices,future_prices = [],[],[],[]
+        # 
         future_date = '2020-05-08'
+        
+        rsi,gc,start_prices,future_prices = [],[],[],[]
         for t in tickers:
             temp = client.get_historicals(t,end=end)       
             future_prices.append(temp.loc[future_date,'Adj Close'])
@@ -79,18 +84,35 @@ class Signal:
         df['RSI'] = rsi
         df['Golden Cross'] = gc
         df['Actual Signal'] = df['End Price'] > df['Start Price']
+        print(df)
         df.drop(['Start Price','End Price'],axis=1,inplace=True)
-        df.to_csv('decision-tree-data/test1.csv')
+        #df.to_csv('decision-tree-data/test1.csv')
         return df
     
     def decision_tree(self):
-        #end = '2020-05-04'
-        #df = self.create_features(end=end)
-        df = pd.read_csv('decision-tree-data/test1.csv',index_col='Ticker')
-
-        model = DecisionTree()
-        model.partition(df)
-        model.gini_index(df)        
+        max_depth = 10
+        min_size = 1
+        
+        df = pd.read_csv('train.csv',index_col='Ticker')
+        df.drop('Golden Cross',axis=1,inplace=True)
+        
+        train = df
+       
+        model = DecisionTree(max_depth)
+        tree = model.build_tree(train,min_size)        
+        
+        for t in list(train.index):
+            pred = model.predict(tree,train.loc[t,:])
+            train.loc[t,'Prediction'] = pred
+            
+        for t in list(test.index):
+            pred = model.predict(tree,test.loc[t,:])
+            test.loc[t,'Prediction'] = pred
+        
+        print('Train Accuracy: %.2f' % (sum(train['Actual Signal'] == train['Prediction'])/len(train)))
+        print('Test Accuracy: %.2f' % (sum(test['Actual Signal'] == test['Prediction'])/len(test)))
+        
+        
 if __name__ == '__main__':
     q = 'SELECT * FROM stocks.collections WHERE Collection = "100-most-popular"'
     df = pd.read_sql(q,con=client.database.connection)
@@ -140,4 +162,7 @@ if __name__ == '__main__':
     
     # Decision Tree
     s = Signal()
-    s.decision_tree()
+    s.create_features()
+    #s.decision_tree()
+    #res = pd.read_csv('decision-tree-data/train_results.csv',index_col='Ticker')
+    #print(sum(res['Actual Signal'] == res['Prediction'])/len(res))
